3bda89972be81440137d9bff555d57a59ddf79ea
Andres Rincon Gomez <krincona@users.noreply.github.com>
2020-10-26 23:34:34 -0500

    adds data requests from API Sample Rick and Morty API

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <div id="app">
     <h1>Coding Test - Torre.co</h1>
+    <button v-on:click="fetch">Find</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "App",
-};
+  data: function () {
+    return {
+      information: []
+    }
+  },
+  methods: {
+    fetch() {
+      //const headers = { "Content-Type": "application/json" };
+      let result = axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then(res => {
+          this.information = res.data.results
+          //console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      }
+    } 
+  };
 </script>
 

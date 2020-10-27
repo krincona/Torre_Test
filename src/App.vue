ff2b0369ac21ade573de1dd8e5a4e659b15b9fbe
Andres Rincon Gomez <krincona@users.noreply.github.com>
2020-10-27 01:29:46 -0500

    include props for User

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,40 @@
 <template>
   <div id="app">
-    <div class="hero is-primary is-bold ">
+    <div class="hero is-black is-bold">
       <div class="hero-body">
-        <div class="title"><span class="is-white">User</span></div>
-        <div class="subtitle"><span class="is-white ">Skills</span></div>
-        <button class="button is-rounded is-info " v-on:click="fetch">Find</button>
+        <div class="title"><span class="is-white">Users</span></div>
+        <div class="subtitle"><span class="is-white">Skills</span></div>
+        <button class="button is-rounded is-info" v-on:click="fetch">
+          Find
+        </button>
       </div>
     </div>
-
+    <span></span>
     <div class="container">
-      <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
-        <div class="column is-12-mobile is-4-desktop is-4-tablet" v-for="user of users" v-bind:key="user.id"> 
-          <div class="card">
-            <div class="card-header"><img v-bind:src="user.image" v-bind:alt="user.name" /></div>
-            <div class="card-content">
-              <h3 class="title is-size-4">{{user.name}}</h3>
-              <button class="button is-rounded is-small is-info">More</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div
+        class="columns is-desktop is-mobile is-tablet is-multiline is-centered"
+      ></div>
     </div>
-  </div>    
+  </div>
 </template>
 
 <script>
 //<div >{{ user }}</div>
 import axios from "axios";
+import User from "./components/User";
 
 export default {
   name: "App",
+  components: {
+    User, // user
+  },
   data: function () {
     return {
       users: [],
     };
+  },
+  create() {
+    this.fetch();
   },
   methods: {
     fetch() {
